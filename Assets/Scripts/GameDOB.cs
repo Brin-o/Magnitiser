0528cba2dc0f37100007d21736aeb7185fab7f0f
tianBreznik <37045546+tianBreznik@users.noreply.github.com>
2020-07-04 00:38:52 +0200

    rapid data sheets

---
+++ Assets/Scripts/GameDOB.cs
@@ -0,0 +1,12 @@
+﻿using Lib.RapidSheetData;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RSDObject]
+public class GameDOB
+{
+    public string Timestamp { get; private set; }
+    public string Username { get; private set; }
+    public string Score { get; private set; }
+}
